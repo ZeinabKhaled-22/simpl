69a99f801d4362c9d531986070ada2b6bfdf5ff6
zeinab khaled <zezekhaled13@gmail.com>
2025-08-14 18:20:08 +0200

    add env file

--- db/connection.js
+++ db/connection.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 // connection
 export const connectDB = async () => {
-   await mongoose.connect('mongodb://localhost:27017/simpl').then(() => {
+   await mongoose.connect(process.env.DB_URL).then(() => {
         console.log('db is connected successfully');
     }).catch((error) => {
         console.log('fail to connect to db', error);
